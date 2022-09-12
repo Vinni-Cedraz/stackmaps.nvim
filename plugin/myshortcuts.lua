96408e55bd6f30274d73e5df5b21d8e157679deb
Vinni-Cedraz <planetexpress0101@gmail.com>
2022-09-12 16:48:41 -0300

    mighty alrighty

--- plugin/myshortcuts.lua
+++ plugin/myshortcuts.lua
@@ -1,7 +1,5 @@
--- the following command keymaps ":colorschemes codemonkey" to the
--- "mn" keys. 
+--- keymaps aurora and codemonkey colorschemes
 vim.api.nvim_set_keymap('n', '<leader>mn', ':colorscheme codemonkey<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>au', ':colorscheme aurora<CR>', { noremap = true, silent = true })
--- the following command keymaps :UndotreeToggle to the "S-u" keys.
-vim.api.nvim_set_keymap('n', '<leader>u', ':UndotreeToggle<CR>', { noremap = true, silent = true })
-
+-- keymaps :UndotreeToggle
+vim.api.nvim_set_keymap('n', '<leader>t', ':UndotreeToggle<CR>', { noremap = true, silent = true })

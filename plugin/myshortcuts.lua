332eec347be39fcd1b67c6ded7165f949ae7d7d5
Vinni <planetexpress0101@gmail.com>
2022-09-11 22:53:52 -0300

    thingies

---
+++ plugin/myshortcuts.lua
@@ -0,0 +1,4 @@
+-- the following command keymaps ":colorschemes codemonkey" to the
+-- "mn" keys. 
+vim.api.nvim_set_keymap('n', '<leader>mn', ':colorscheme codemonkey<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>au', ':colorscheme aurora<CR>', { noremap = true, silent = true })
